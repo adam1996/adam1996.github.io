bfdf624b124566ed38676698b324fff5ebc62496
Adam Johnson <adam7819@googlemail.com>
2023-07-03 20:37:35 +0100

    convert from prompt to buttons

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,52 +1,63 @@
-let playerScore = 0
-let computerScore = 0
-
-let getComputerChoice = function(){
-    let choices = ['Paper' , 'Scissors' , 'Rock'];
-    let i = Math.floor(Math.random() * 3);
-    return choices[i].toLowerCase();
-}
-
-let playGame = function(playerSelection, computerSelection) {
-    const winningCombinations = {
+document.addEventListener('DOMContentLoaded', function() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    const resultText = document.getElementById('result-text');
+    const buttons = document.querySelectorAll('.user-choice');
+  
+    let getComputerChoice = function() {
+      let choices = ['paper', 'scissors', 'rock'];
+      let i = Math.floor(Math.random() * 3);
+      return choices[i];
+    };
+  
+    let playGame = function(playerSelection, computerSelection) {
+      const winningCombinations = {
         'rock': 'scissors',
         'scissors': 'paper',
         'paper': 'rock'
-    };
-
-
-    if (playerSelection === computerSelection) {
+      };
+  
+      if (playerSelection === computerSelection) {
         playerScore += 0.5;
         computerScore += 0.5;
         return 'Draw';
-    }
-
-    if (winningCombinations[playerSelection] === computerSelection) {
+      }
+  
+      if (winningCombinations[playerSelection] === computerSelection) {
         playerScore += 1;
         return 'Player wins';
-        
-    } else {
+      } else {
         computerScore += 1;
         return 'Computer wins';
-    }
-}
-
-let game = function(numberOfRound) {
-    for (i = 0; i <= numberOfRound; i++){
-        let playerSelection = prompt('What is your choice - rock, paper, or scissors?');
-        let computerSelection = getComputerChoice();
-
-        let result = playGame(playerSelection, computerSelection);
-        console.log(result)
-        console.log('Player Score: ' + playerScore + 'Computer Score: ' + computerScore);
-    }
-    if (playerScore === computerScore) {
-        console.log('DRAW')
-    }
-    
-    playerScore > computerScore ? console.log('You have won') : console.log('The computer has won');
-}
-
-
-
-game(1)
+      }
+    };
+  
+    let updateScore = function() {
+      resultText.textContent = 'Player Score: ' + playerScore + '  Computer Score: ' + computerScore;
+    };
+  
+    let resetScores = function() {
+      playerScore = 0;
+      computerScore = 0;
+      updateScore();
+    };
+  
+    let game = function() {
+      updateScore();
+  
+      buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+          let playerSelection = this.id;
+          let computerSelection = getComputerChoice();
+  
+          let result = playGame(playerSelection, computerSelection);
+          console.log(result);
+          updateScore();
+        });
+      });
+    };
+  
+    game();
+  });
+  
