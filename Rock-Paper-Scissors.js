a2c75e9168f85ceb4f12ce867c3fb94afc6c3439
Adam Johnson <adam7819@googlemail.com>
2023-07-03 20:56:19 +0100

    Add ID selectors to target buttons

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const resultText = document.getElementById('result-text');
     const buttons = document.querySelectorAll('.user-choice');
   
+    /* Generate computer choice */
     let getComputerChoice = function() {
       let choices = ['paper', 'scissors', 'rock'];
       let i = Math.floor(Math.random() * 3);
       return choices[i];
     };
-  
+    
+    /* Round logic updates score vars */
     let playGame = function(playerSelection, computerSelection) {
       const winningCombinations = {
         'rock': 'scissors',
@@ -32,27 +34,30 @@ document.addEventListener('DOMContentLoaded', function() {
         return 'Computer wins';
       }
     };
-  
+    
+    /* Update result-text DIV with current game score: */
     let updateScore = function() {
       resultText.textContent = 'Player Score: ' + playerScore + '  Computer Score: ' + computerScore;
     };
-  
+    
+    /* DEFUNCT - ADD reset button needed: */
     let resetScores = function() {
       playerScore = 0;
       computerScore = 0;
       updateScore();
     };
   
+    /* MAIN - */
     let game = function() {
       updateScore();
-  
       buttons.forEach(function(button) {
         button.addEventListener('click', function() {
-          let playerSelection = this.id;
+          let playerSelection = this.id;  //referrs to the id value of the img which in turn translates to userSelection 
+          console.log(playerSelection + 'TEST POINT')
           let computerSelection = getComputerChoice();
   
           let result = playGame(playerSelection, computerSelection);
-          console.log(result);
+          console.log(result); //Remove
           updateScore();
         });
       });
