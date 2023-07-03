ba490f2c158fdd7b60379754fbc6aa41cf3f89cf
Adam Johnson <adam7819@googlemail.com>
2023-07-03 18:49:22 +0100

    Redeploy

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let computerScore = 0
+
 let getComputerChoice = function(){
     let choices = ['Paper' , 'Scissors' , 'Rock'];
     let i = Math.floor(Math.random() * 3);
@@ -11,13 +14,19 @@ let playGame = function(playerSelection, computerSelection) {
         'paper': 'rock'
     };
 
+
     if (playerSelection === computerSelection) {
+        playerScore += 0.5;
+        computerScore += 0.5;
         return 'Draw';
     }
 
     if (winningCombinations[playerSelection] === computerSelection) {
+        playerScore += 1;
         return 'Player wins';
+        
     } else {
+        computerScore += 1;
         return 'Computer wins';
     }
 }
@@ -28,9 +37,16 @@ let game = function(numberOfRound) {
         let computerSelection = getComputerChoice();
 
         let result = playGame(playerSelection, computerSelection);
-        console.log(result);
+        console.log(result)
+        console.log('Player Score: ' + playerScore + ' Computer Score: ' + computerScore);
+    }
+    if (playerScore === computerScore) {
+        console.log('DRAW')
     }
 
+    playerScore > computerScore ? console.log('You have won') : console.log('The computer has won');
 }
+
+
 
 game(5)
